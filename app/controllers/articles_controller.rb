3c8771dc5f574679e5fa2d7d0b67b4a929ddf839
smarzz <marzaric@gmail.com>
2017-08-08 15:05:42 +0000

    add partials and add links to index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -45,6 +45,14 @@ class ArticlesController < ApplicationController
         
     end
     
+    def destroy
+       @article = Article.find(params[:id]) 
+       @article.destroy
+       flash[:notice] = "Article was succsesfully deleted"
+       redirect_to articles_path
+        
+    end
+    
     
     private
         def article_params
